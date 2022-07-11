f3f6baa4a5962580dd9f459c9911be9c5325a180
Neeraj-tech <neerajagarwal666@gmail.com>
2022-07-11 08:02:21 +0530

    update

--- terraform1.tf
+++ terraform1.tf
@@ -5,6 +5,8 @@ profile = "default"
 resource "aws_instance" "controller" {
 ami = "ami-068257025f72f470d"
 instance_type = "t2.micro"
+key_name = "EC2new"
+security_groups = ["launch-wizard-2"]
 tags = {
 Name = "Controller"
 }
