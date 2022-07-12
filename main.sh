7a64b2b4ca3c29fa3646a2db2d1832f3558716dc
Neeraj-tech <neerajagarwal666@gmail.com>
2022-07-12 14:45:26 +0530

    update

--- main.sh
+++ main.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 cd ..
+echo "Installing Terraform and Java.................................................."
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update && sudo apt-get install terraform
 sudo apt-get install fontconfig openjdk-11-jre -y
 sudo apt update
-sudo apt install software-properties-common
+echo "Installing Ansible, AWS Cli and jq.................................................."
+sudo apt install software-properties-common -y
 sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
-sudo apt install awscli
-cat "installtion of Terraform java Ansible to current system completed."
-cat "To launch instance choose right main.tf in main1.sh it will further lead the process."
+sudo apt install ansible -y
+sudo apt install awscli -y
+sudo apt install jq -y
+echo "installtion of Terraform java Ansible to current system completed."
+echo "To launch instance choose right main.tf in main1.sh it will further lead the process."
--- main1.sh
+++ main1.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-mv ./main.tf ./../terraform/
+cd ..
+echo "Please provide AWS credential for progmmatic acces with terraform..................."
+aws configure
+mv ./system_setup/main.tf ./terraform/
+cd terraform/
+echo "Initializing Terraform..........................................."
+terraform init
 terraform apply
-terraform output controller.controller_ip | jq -r > ipfile
+terraform output controller_ip | jq -r > ipfile
 export x=$(<ipfile)
-print "%s user=ubuntu key=EC2new" "$x" > hosts
+echo "Note: EC2key file name will be EC2new.pem............................"
+printf "%s ansible_user=ubuntu ansible_ssh_private_key_file=/etc/ansible/EC2new.pem" $x > hosts
+cat hosts >> /etc/ansible/hosts
+cd /etc/ansible/
+echo "Provide EC2new.pem file content...................after copy paste save (ctrl+o) & exit(ctrl+x)"
+nano EC2new.pem
+chmod 400 EC2new.pem
+ansible all -m ping
